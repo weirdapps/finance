52ad80c8d23018e14d08c6ad9f7ac6f87ed4fbcb
Dimitris Plessas <plessasdimitrios@yahoo.com>
2023-05-23 11:02:55 +0300

    include market cap data

--- main.py
+++ main.py
@@ -1,96 +1,92 @@
-# first import the libraries
-
 import matplotlib.pyplot as plt
-from scipy.optimize import minimize
 import numpy as np
-import yfinance as yf
 import pandas as pd
+import yfinance as yf
+from scipy.optimize import minimize
 
-# read the csv file
-df = pd.read_csv('/Users/plessas/Downloads/marketcap.csv')
+# Set the file path for the CSV
+csv_path = 'marketcap.csv'
 
-# convert the csv file to a list
-symbol_list = df['Symbol'].tolist()
-symbol_list = symbol_list[0:100]
+# Read the CSV file into a DataFrame
+df = pd.read_csv(csv_path)
 
-# connect to yahoo finance and download daily adjusted close prices for symbol_list
-data = yf.download(symbol_list, start="2020-01-01", end="2023-05-20")
+# Select the symbol column and convert it to a list
+symbol_list = df['Symbol'].tolist()[:20]
 
-# calculate the daily returns
-daily_returns = data['Adj Close'].pct_change()
+# Download daily adjusted close prices for symbol_list from Yahoo Finance
+data = yf.download(symbol_list, start="2021-01-01",
+                   end="2023-05-20")["Adj Close"]
 
-# optimize the portfolio by maximizing the sharpe ratio
+# Calculate the daily returns
+daily_returns = data.pct_change()
+
+# Optimize the portfolio by maximizing the Sharpe ratio
 
 
-def get_ret_vol_sr(weights):
-    """
-    Takes in weights, returns array or return, volatility, sharpe ratio
-    """
+def get_ret_vol_sr(weights, returns):
     weights = np.array(weights)
-    ret = np.sum(daily_returns.mean() * weights) * 252
-    vol = np.sqrt(np.dot(weights.T, np.dot(
-        daily_returns.cov() * 252, weights)))
-    sr = ret/vol
-    return np.array([ret, vol, sr])
+    ret = np.sum(returns.mean() * weights) * 252
+    vol = np.sqrt(np.dot(weights.T, np.dot(returns.cov() * 252, weights)))
+    sr = ret / vol
+    return ret, vol, sr
 
 
-def neg_sharpe(weights):
-    return get_ret_vol_sr(weights)[2] * -1
+def neg_sharpe(weights, returns):
+    return -get_ret_vol_sr(weights, returns)[2]
 
 
 def check_sum(weights):
     return np.sum(weights) - 1
 
 
+def minimize_volatility(weights, returns):
+    return get_ret_vol_sr(weights, returns)[1]
+
+
+# Set the optimization constraints and bounds
 cons = ({'type': 'eq', 'fun': check_sum})
-
 bounds = ((0, 1),) * len(symbol_list)
+init_guess = [1 / len(symbol_list)] * len(symbol_list)
 
-init_guess = [1/len(symbol_list)] * len(symbol_list)
+# Optimize the portfolio for the maximum Sharpe ratio
+opt_results = minimize(neg_sharpe, init_guess, args=(
+    daily_returns,), method='SLSQP', bounds=bounds, constraints=cons)
 
-opt_results = minimize(neg_sharpe, init_guess,
-                       method='SLSQP', bounds=bounds, constraints=cons)
-
-opt_results
-
-get_ret_vol_sr(opt_results.x)
-
-# calculate the efficient frontier
+# Calculate the efficient frontier
 frontier_y = np.linspace(0, 0.3, 100)
-
-
-def minimize_volatility(weights):
-    return get_ret_vol_sr(weights)[1]
-
-
 frontier_volatility = []
 
 for possible_return in frontier_y:
     cons = ({'type': 'eq', 'fun': check_sum},
-            {'type': 'eq', 'fun': lambda w: get_ret_vol_sr(w)[0] - possible_return})
-
-    result = minimize(minimize_volatility, init_guess,
-                      method='SLSQP', bounds=bounds, constraints=cons)
-
+            {'type': 'eq', 'fun': lambda w: get_ret_vol_sr(w, daily_returns)[0] - possible_return})
+    result = minimize(minimize_volatility, init_guess, args=(
+        daily_returns,), method='SLSQP', bounds=bounds, constraints=cons)
     frontier_volatility.append(result['fun'])
 
+# Plot the efficient frontier
 plt.figure(figsize=(12, 8))
 plt.scatter(frontier_volatility, frontier_y, c=frontier_y /
             frontier_volatility, cmap='viridis')
 plt.colorbar(label='Sharpe Ratio')
 plt.xlabel('Volatility')
 plt.ylabel('Return')
+plt.title('Efficient Frontier')
 plt.show()
 
-# print a table of the stock symbols, the company names and the optimal weights in percent with two decimal places
-optimal_weights = opt_results.x
-optimal_weights = optimal_weights * 100
-optimal_weights = optimal_weights.round(2)
-optimal_weights = pd.DataFrame(optimal_weights, columns=['Optimal Weights'])
-optimal_weights['Symbol'] = symbol_list
-optimal_weights['Company Name'] = df['Name']
-optimal_weights = optimal_weights[[
-    'Symbol', 'Company Name', 'Optimal Weights']]
-optimal_weights = optimal_weights.sort_values(
-    by=['Optimal Weights'], ascending=False)
+# Get the optimal weights and sort them
+optimal_weights = opt_results.x * 100
+optimal_weights = pd.DataFrame(optimal_weights, columns=[
+                               'Optimal Weights'], index=symbol_list[:len(optimal_weights)])
+optimal_weights['Optimal Weights'] = optimal_weights['Optimal Weights'].map(
+    '{:.2f}%'.format)
+optimal_weights.sort_values(
+    by='Optimal Weights', ascending=False, inplace=True)
+
+# Print the table of optimal weights
 print(optimal_weights)
+
+# Print the expected annual return, annual volatility, and Sharpe ratio
+ret, vol, sr = get_ret_vol_sr(opt_results.x, daily_returns)
+print('Expected Annual Return:', round(ret * 100, 2), '%')
+print('Annual Volatility:', round(vol * 100, 2), '%')
+print('Sharpe Ratio:', round(sr, 2))
